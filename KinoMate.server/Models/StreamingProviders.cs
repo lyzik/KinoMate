157aa167b70ed0b803d0c3a00b5bc5cacfdb38cc
gloczek18 <k.boguslawski21.kb@gmail.com>
2025-01-29 16:37:36 +0100

    streaming providers

---
+++ KinoMate.server/Models/StreamingProviders.cs
@@ -0,0 +1,9 @@
+﻿namespace KinoMate.server.Models
+{
+    public class StreamingProviders
+    {
+        public string Name { get; set; }
+        public string LogoPath { get; set; }
+        public string Link { get; set; }
+    }
+}
---
+++ KinoMate.server/Models/WatchProvidersResponse.cs
@@ -0,0 +1,22 @@
+﻿using System.Text.Json.Serialization;
+
+public class WatchProvidersResponse
+{
+    [JsonPropertyName("results")]
+    public Dictionary<string, WatchProviderCountry> Results { get; set; }
+}
+
+public class WatchProviderCountry
+{
+    [JsonPropertyName("flatrate")]
+    public List<StreamingProvider> Flatrate { get; set; }
+}
+
+public class StreamingProvider
+{
+    [JsonPropertyName("provider_name")]
+    public string ProviderName { get; set; }
+
+    [JsonPropertyName("logo_path")]
+    public string LogoPath { get; set; }
+}
